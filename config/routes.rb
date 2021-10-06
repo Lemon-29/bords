851836d245ece2941ff04f470968dbe24b3d1458
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-10-06 14:00:04 +0900

    refactoring for rooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'bords', to: 'bords#index'
-  get 'bords/new', to: 'bords#new'
-  post 'bords', to: 'bords#create'
-  get 'bords/:id', to: 'bord#show'
+  resources :bords
 end
