ecd7e33ccd11ef43ab58461293981020369f03f9
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-10-22 20:28:26 +0900

    change migration file for comments

--- db/migrate/20211022112645_create_comments.rb
+++ db/migrate/20211022112645_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
       t.references :bord, foreign_key: true
-      t.string :name
-      t.text :comment
+      t.string :name, null: false
+      t.text :comment, null: false
 
       t.timestamps
     end
