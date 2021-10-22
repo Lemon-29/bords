7084b20c60daf8665a7b264f0e8d1c68e0a58481
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-10-22 20:30:26 +0900

    set association

--- app/models/bord.rb
+++ app/models/bord.rb
@@ -1,4 +1,5 @@
 class Bord < ApplicationRecord
+  has_many :commnts
   validates :name, presence: true, length: { maxim:10}
   validates :title, presence:true, length: { maximum: 30}
   validates :body, presence: true, length: { maximum: 1000}
