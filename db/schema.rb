2232d300824b67b35e7a49848ba4924be4d05643
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-10-23 21:43:54 +0900

    fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_22_075833) do
+ActiveRecord::Schema.define(version: 2021_10_22_112645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(version: 2021_10_22_075833) do
 
   create_table "comments", force: :cascade do |t|
     t.bigint "bord_id"
-    t.string "name"
-    t.text "comment"
+    t.string "name", null: false
+    t.text "comment", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["bord_id"], name: "index_comments_on_bord_id"
