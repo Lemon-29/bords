bd3646f9c86945fd7560db2bb53d2d7443409cd9
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-10-24 19:56:16 +0900

    set rooting for comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'comments/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :bords
+  resources :comments, only: %i[create destroy]
 end
