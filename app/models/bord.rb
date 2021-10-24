46e669a670e33de48e667fc92845506b7a2bc2c3
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-10-24 20:59:11 +0900

    fix misspaling

--- app/models/bord.rb
+++ app/models/bord.rb
@@ -1,6 +1,6 @@
 class Bord < ApplicationRecord
   has_many :commnts
-  validates :name, presence: true, length: { maxim:10}
+  validates :name, presence: true, length: { maximum:10}
   validates :title, presence:true, length: { maximum: 30}
   validates :body, presence: true, length: { maximum: 1000}
 end
