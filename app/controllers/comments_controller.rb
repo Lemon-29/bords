61e86101656075a2ee73862e5fcc4852a6e807d6
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-10-27 15:08:15 +0900

    set new comments_form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,16 @@
 class CommentsController < ApplicationController
   def create
+    comment = Comment.new(comment_params)
+  if comment.save
+     flash[:notice]= ' コメントを投稿しました。'
+     redirect_to comment.bord
+  else
+    redirect_to :back, flash: {
+      comment: comment,
+      error_messages: comment.errors.full_messages
+    }
   end
+end
 
   def destroy
   end
