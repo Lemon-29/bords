6cc2de428563ac2605fb7f7e20db90d668f7c080
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-11-01 12:11:35 +0900

    set dependency

--- app/models/bord.rb
+++ app/models/bord.rb
@@ -1,8 +1,8 @@
 class Bord < ApplicationRecord
-  has_many :commnts
-  has_many :bord_tag_relations
+  has_many :commnts, dependent: :delete_all
+  has_many :bord_tag_relations, dependent: :delete_all
   has_many :tags, through: :bord_tag_relations
-  
+
   validates :name, presence: true, length: { maximum:10}
   validates :title, presence:true, length: { maximum: 30}
   validates :body, presence: true, length: { maximum: 1000}
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ApplicationRecord
-  has_many :bord_tag_relations
+  has_many :bord_tag_relations, dependent: :delete_all
   has_many :bords, through: :bord_tag_relations
 end
