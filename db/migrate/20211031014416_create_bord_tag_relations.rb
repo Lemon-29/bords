2ed4431522aee9a77060a4a26ddb94f6c72850c0
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-11-01 21:53:50 +0900

    fix missspeling

--- db/migrate/20211031014416_create_bord_tag_relations.rb
+++ db/migrate/20211031014416_create_bord_tag_relations.rb
@@ -1,7 +1,7 @@
 class CreateBordTagRelations < ActiveRecord::Migration[5.2]
   def change
     create_table :bord_tag_relations do |t|
-      t.referencese :bord
+      t.references :bord
       t.references :tag, foreign_key: true
 
       t.timestamps
