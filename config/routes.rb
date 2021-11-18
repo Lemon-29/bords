1f5ef713404ab37ad5711e62eb164a2211b2deeb
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-11-18 17:06:39 +0900

    set header_menu & helper_method

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'comments/create'
-  get 'comments/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
   resources :bords
   resources :comments, only: %i[create destroy]
 end
