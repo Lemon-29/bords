66df0e4bbf2389b9208e6317da013766cc29ce3f
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-11-20 10:57:46 +0900

    set save for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    @user = User.new(flash[:user])
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.:id
+      redirect_to mypage_path
+    else
+      redirect_to :back, flash: {
+        user: user,
+        error_messages: user.errors.full_messages
+      }
+    end
   end
 
   def me
